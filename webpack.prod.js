352725625dbbd5f0f52df48487cabb3b4f5fb6fb
Eugen Schreiner <eugen.schreiner@bertelsmann.de>
2023-04-19 22:04:57 +0200

    add service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -49,6 +50,8 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: "[name].css",
+        }),
+        new WorkboxPlugin.GenerateSW({
         }),
     ]
 }
