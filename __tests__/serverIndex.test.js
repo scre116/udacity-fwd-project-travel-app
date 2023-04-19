fcd6b05b42e98894872ab6430b2bc9239d992614
Eugen Schreiner <eugen.schreiner@bertelsmann.de>
2023-04-19 21:54:03 +0200

    test /analyze

---
+++ __tests__/serverIndex.test.js
@@ -0,0 +1,59 @@
+const request = require('supertest');
+const app = require('../src/server/index');
+global.fetch = require('jest-fetch-mock');
+
+describe('GET /', () => {
+    it('should respond with index.html', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toContain("<title>Sentiment Analysis</title>");
+    });
+});
+
+describe('GET /analyze', () => {
+    it('should response with analysis', async () => {
+        fetch.mockResponseOnce(JSON.stringify({
+            agreement: 'AGREEMENT',
+            confidence: '100',
+            irony: 'NONIRONIC',
+            model: 'general_en',
+            score_tag: 'NONE',
+            sentence_list: [{
+                whatever: 'whatever',
+            }],
+            sentimented_concept_list: [],
+            sentimented_entity_list: [],
+            status: {code: '0', msg: 'OK', credits: '1', remaining_credits: '19956'},
+            subjectivity: 'OBJECTIVE'
+        }));
+
+        const response = await request(app).get('/analyze?text=test');
+        expect(fetch).toHaveBeenCalledWith(expect.stringMatching(/https:\/\/api\.meaningcloud\.com\/sentiment-2\.1\?key=.*&lang=en&egp=y&txt=test/));
+        expect(response.statusCode).toBe(200);
+        expect(response.body.polarity).toBe('Without Polarity');
+        expect(response.body.subjectivity).toBe('Objective');
+        expect(response.body.irony).toBe('Nonironic');
+        expect(response.body.agreement).toBe('Agreement');
+        expect(response.body.confidence).toBe('100');
+
+    });
+    
+    it('should response with error if status code is not 0',  async  () => {
+        fetch.mockResponseOnce(JSON.stringify({
+            status: {code: '1', msg: 'ERROR MSG', credits: '1', remaining_credits: '19956'},
+        }));
+
+        const response = await request(app).get('/analyze?text=test');
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({"error": "ERROR MSG"});
+    });
+    
+    it('should response with error if fetch fails',  async  () => {
+        fetch.mockRejectOnce('some error');
+
+        const response = await request(app).get('/analyze?text=test');
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({"error": "some error"});
+    });
+
+});
